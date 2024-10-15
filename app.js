1f5bfd3d54b9c0c96c9ade9087af7459c3bdaf61
Julien Flusin <julienflusin3@gmail.com>
2024-10-15 10:07:05 +0200

    auto select exposed uri

--- app.js
+++ app.js
@@ -3,23 +3,32 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./api/swagger.json');
 const app = express();
 const port = parseInt(process.env.API_PORT) || 3000;
+
+// Determine environment
+const env = process.env.ENV || 'dev';
+const swaggerDocument = require('./api/swagger.json');
+
+// Set the correct host based on the environment
+if (env === 'dev') {
+    swaggerDocument.host = `localhost:${port}`;
+    swaggerDocument.schemes = ['http'];
+} else if (env === 'prod') {
+    swaggerDocument.host = 'safe-place.flusin.fr';
+    swaggerDocument.schemes = ['https'];
+}
 
 //// DOCS ROUTE ////
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //// DOCS ROUTE ////
 
 
-
 //// API ROUTES ////
 const helloWorldRoute = require('./api/routes/hello-world');
 
 app.use('/api/hello-world', helloWorldRoute);
-
 //// API ROUTES ////
-
 
 
 //// FRONT-END ROUTES ////
@@ -29,8 +38,8 @@ app.use('/', homepageRoute);
 //// FRONT-END ROUTES ////
 
 
-
 // APP LISTEN //
 app.listen(port, () => {
-    console.log(`Safe place api is listening at http://localhost:${port}/api`)
-})
+    const baseUrl = env === 'prod' ? 'https://safe-place.flusin.fr' : `http://localhost:${port}`;
+    console.log(`Safe place API is listening at ${baseUrl}/api`);
+});
